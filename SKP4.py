5855dba091ab2af08102812efe28508ac7d59b6f
Annisa-Maulida-Rizky <annisamaulida1klyh@gmail.com>
2026-09-15 20:19:28 +0800

    Add files via upload

---
+++ SKP4.py
@@ -0,0 +1,43 @@
+buku = {
+    "Judul" : "Bumi",
+    "Penulis" : "Tere Liye",
+    "Tahun Terbit" : "2014"
+}
+
+while True:
+    print("Menu: ")
+    print("1. Tampilkan data buku")
+    print("2. Tambahkan Penerbit")
+    print("3. Ubah data Penulis")
+    print("4. Hapus data Penerbit")
+
+    Pilih_menu = input("Pilih menu(ketik keluar untuk berhenti): ")
+
+    if Pilih_menu == "keluar":
+        break
+
+    if Pilih_menu == "1":
+
+        print("Berikut adalah data buku: ")
+        print(buku)
+
+    elif Pilih_menu == "2":
+        buku["Penerbit"] = "Gramedia Pustaka Utama"
+
+        print("Setelah ditambahkan: ")
+        print(buku)
+
+    elif Pilih_menu == "3":
+        buku.update({"Penulis" : "Darwis"})
+
+        print("Setelah diupdate: ")
+        print(buku)
+
+    elif Pilih_menu == "4":
+        buku.pop("Penerbit")
+
+        print("Setelah dihapus: ")
+        print(buku)
+
+    else:
+        print("Pilihan tidak ada. Harap pilih ulang sesuai menu")
